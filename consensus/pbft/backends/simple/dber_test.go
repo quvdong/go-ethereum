7c941603139127b3a604bb28b7d021e1f59c4a3b
mark.lin <mark@maicoin.com>
2017-04-19 14:39:00 +0800

    consensus/pbft/backends/simple: add dber to save data into db

---
+++ consensus/pbft/backends/simple/dber_test.go
@@ -0,0 +1,51 @@
+package simple
+
+import (
+	"math/big"
+	"os"
+	"path/filepath"
+	"reflect"
+	"testing"
+
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/ethdb"
+)
+
+type Foo struct {
+	Name   string
+	Number *big.Int
+	Hash   common.Hash
+}
+
+func TestSaveAndRestore(t *testing.T) {
+	db := newDBer(newTestDb())
+	key := "key"
+	foo := Foo{
+		Name:   "name",
+		Number: big.NewInt(9),
+		Hash:   common.HexToHash("1234567890"),
+	}
+	err := db.Save(key, foo)
+	if err != nil {
+		t.Error("Should save the object correctly")
+	}
+
+	result := &Foo{}
+	err = db.Restore(key, result)
+	if err != nil {
+		t.Error("Should restore the object correctly")
+	}
+	if reflect.DeepEqual(foo, result); result.Name != foo.Name {
+		t.Errorf("Should have the same data, result = %v, expected = %v", result, foo)
+	}
+}
+
+func newTestDb() ethdb.Database {
+	file := filepath.Join("/", "tmp", "pbfttesttmpfile")
+	if common.FileExist(file) {
+		os.RemoveAll(file)
+	}
+	db, _ := ethdb.NewLDBDatabase(file, 0, 0)
+
+	return db
+}
