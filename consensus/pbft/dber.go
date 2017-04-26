c6fbe1c939ab2a9f9752ca66e3bedc8a9f505913
Alan Chen <alan@maicoin.com>
2017-04-26 14:28:29 +0800

    consensus/pbft: remove old DBer interface and add persistence test for backend

--- consensus/pbft/dber.go
+++
@@ -1,8 +0,0 @@
-package pbft
-
-type Dber interface {
-	// Save an object into db
-	Save(key string, val interface{}) error
-	// Restore an object to val from db
-	Restore(key string, val interface{}) error
-}
