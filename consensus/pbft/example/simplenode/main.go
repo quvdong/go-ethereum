161f9988152ade256f898cb2312ed776b2b91921
Miya Chen <jumiya2@gmail.com>
2017-04-27 16:45:07 +0800

    consensus/pbft: make pbft simulation based on simple backend

--- consensus/pbft/example/simplenode/main.go
+++ consensus/pbft/example/simplenode/main.go
@@ -17,15 +17,11 @@
 package main
 
 import (
-	"fmt"
-	"math/big"
 	"os"
-	"sync"
 	"time"
 
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/consensus/pbft/backends/simulation"
-	pbftCore "github.com/ethereum/go-ethereum/consensus/pbft/core"
 	"github.com/ethereum/go-ethereum/core"
 	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/log"
@@ -47,28 +43,29 @@ func main() {
 	glogger.Verbosity(log.LvlDebug)
 	log.Root().SetHandler(glogger)
 
-	// 1. Setup validators and backends
-	var validators = make([]pbftCore.Engine, N)
-	var backends = make([]*simulation.Backend, N)
-
+	var nodeKeys = make([]*simulation.NodeKey, N)
+	var addrs = make([]common.Address, N)
 	for i := 0; i < N; i++ {
-		backend := simulation.NewBackend(uint64(i))
-		backend.Start(nil)
-		defer backend.Stop()
-		validator := pbftCore.New(backend)
-		validator.Start()
-		defer validator.Stop()
+		nodeKeys[i] = simulation.NewNodeKey()
+		addrs[i] = nodeKeys[i].Address()
+	}
 
-		validators[i] = validator
-		backends[i] = backend
-		log.Info("Backend:", "Index", i, "Address", backend.Address().Hex())
+	// generate genesis block
+	defaultGenesis, _ := core.DefaultGenesisBlock().ToBlock()
+	genesisBlock := simulation.AppendValidators(defaultGenesis.Header(), addrs)
+
+	var backends = make([]*simulation.ProtocolManager, N)
+	for i := 0; i < N; i++ {
+		backends[i] = simulation.New(nodeKeys[i], genesisBlock)
+		backends[i].Start()
+		defer backends[i].Stop()
 	}
 
 	// 2. Add peers for each backend
 	for i := 0; i < N; i++ {
 		for j := 0; j < N; j++ {
 			if i != j {
-				backends[i].AddPeer(fmt.Sprintf("%v", j), nil)
+				backends[i].AddPeer(backends[j].SelfPeer())
 			}
 		}
 	}
@@ -76,51 +73,7 @@ func main() {
 	log.Info("Start")
 
 	time.Sleep(3 * time.Second)
-
-	genesisBlock, _ = core.DefaultGenesisBlock().ToBlock()
-	block := genesisBlock
-
-	blocks := make(map[common.Hash]*types.Block)
-	blocksMu := new(sync.Mutex)
-	blocks[block.Hash()] = block
-
-	// 3. Subscribe CommitEvent for each backend
 	for _, backend := range backends {
-		subscription := backend.EventMux().Subscribe(simulation.CommitEvent{})
-		be := backend
-		go func() {
-			for event := range subscription.Chan() {
-				switch ev := event.Data.(type) {
-				case simulation.CommitEvent:
-					blocksMu.Lock()
-					b := blocks[common.BytesToHash(ev.Payload)]
-					blocksMu.Unlock()
-					log.Info("Block committed", "number", b.NumberU64(), "hash", b.Hash().String(), "address", be.Address().Hex())
-				}
-			}
-		}()
+		backend.TryNewRequest()
 	}
-
-	// 4. Backend[0] (primary) requests a new block every minute
-	for {
-		backends[0].NewRequest(block.Hash().Bytes())
-		block = makeBlock(block, block.Number())
-		blocksMu.Lock()
-		blocks[block.Hash()] = block
-		blocksMu.Unlock()
-		time.Sleep(1 * time.Second)
-	}
-}
-
-func makeBlock(parent *types.Block, num *big.Int) *types.Block {
-	header := &types.Header{
-		ParentHash: parent.Hash(),
-		Number:     num.Add(num, common.Big1),
-		GasLimit:   new(big.Int),
-		GasUsed:    new(big.Int),
-		Extra:      nil,
-		Time:       big.NewInt(int64(time.Now().Nanosecond())),
-	}
-
-	return types.NewBlockWithHeader(header)
 }
