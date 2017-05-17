d327b01d3e3bd458e70f2982d771894fb8ba8851
Edwin <edwin@maicoin.com>
2017-05-17 17:08:28 +0800

    consensus/pbft: fix pr comments

--- consensus/pbft/config.go
+++ consensus/pbft/config.go
@@ -33,7 +33,7 @@ type Config struct {
 var DefaultConfig = &Config{
 	RequestTimeoutMsec: 2000,
 	BlockPeriod:        1,
-	BlockPauseTime:     1,
+	BlockPauseTime:     2,
 	ProposerPolicy:     RoundRobin,
 	CheckPointPeriod:   1,
 }
