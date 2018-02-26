826ef82c8ff24b88c929b1800d84a031a8683172
mark.lin <mark@maicoin.com>
2018-02-26 20:17:06 +0800

    consenses/istanbul: remove go metrics

--- consensus/istanbul/core/core.go
+++ consensus/istanbul/core/core.go
@@ -29,7 +29,6 @@ import (
 	"github.com/ethereum/go-ethereum/event"
 	"github.com/ethereum/go-ethereum/log"
 	metrics "github.com/ethereum/go-ethereum/metrics"
-	goMetrics "github.com/rcrowley/go-metrics"
 	"gopkg.in/karalabe/cookiejar.v2/collections/prque"
 )
 
@@ -47,9 +46,9 @@ func New(backend istanbul.Backend, config *istanbul.Config) Engine {
 		pendingRequests:    prque.New(),
 		pendingRequestsMu:  new(sync.Mutex),
 		consensusTimestamp: time.Time{},
-		roundMeter:         metrics.NewMeter("consensus/istanbul/core/round"),
-		sequenceMeter:      metrics.NewMeter("consensus/istanbul/core/sequence"),
-		consensusTimer:     metrics.NewTimer("consensus/istanbul/core/consensus"),
+		roundMeter:         metrics.NewRegisteredMeter("consensus/istanbul/core/round", nil),
+		sequenceMeter:      metrics.NewRegisteredMeter("consensus/istanbul/core/sequence", nil),
+		consensusTimer:     metrics.NewRegisteredTimer("consensus/istanbul/core/consensus", nil),
 	}
 	c.validateFn = c.checkValidatorSignature
 	return c
@@ -87,11 +86,11 @@ type core struct {
 
 	consensusTimestamp time.Time
 	// the meter to record the round change rate
-	roundMeter goMetrics.Meter
+	roundMeter metrics.Meter
 	// the meter to record the sequence update rate
-	sequenceMeter goMetrics.Meter
+	sequenceMeter metrics.Meter
 	// the timer to record consensus duration (from accepting a preprepare to final committed stage)
-	consensusTimer goMetrics.Timer
+	consensusTimer metrics.Timer
 }
 
 func (c *core) finalizeMessage(msg *message) ([]byte, error) {
